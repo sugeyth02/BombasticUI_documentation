bca95ec9cb43ba4e000bbba8712ac874fe7854a7
Sugeyth Rodriguez <d.sugeyth02@gmail.com>
2023-06-13 15:21:45 -0600

    :bug: Fix ts config

--- src/styles/Button.ts
+++ src/styles/Button.ts
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { theme } from './Theme';
+import { theme } from './theme';
 
 export const PrimaryButton = styled.button`
  width: fit-content;
--- src/styles/Typography.ts
+++ src/styles/Typography.ts
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { theme } from './Theme';
+import { theme } from './theme';
 
 export const PrimaryTitle = styled.h1`
    font-family: 'Montserrat';
